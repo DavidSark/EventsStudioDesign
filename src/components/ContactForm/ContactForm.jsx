cb576646c43c4b6734fe680f8f7f35632f40b229
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-05-13 04:59:24 +0200

    suite form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,127 +1,53 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react'
+import './ContactForm.scss'
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
-  });
-  const [formErrors, setFormErrors] = useState({});
-  const [formSubmitted, setFormSubmitted] = useState(false);
-
-  const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const errors = validateForm(formData);
-    setFormErrors(errors);
-    if (Object.keys(errors).length === 0) {
-      sendEmail();
-      setFormData({
-        name: '',
-        email: '',
-        subject: '',
-        message: ''
-      });
-      setFormSubmitted(true);
-    }
-  };
-
-  const validateForm = (data) => {
-    let errors = {};
-    if (!data.name.trim()) {
-      errors.name = 'Le nom est requis.';
-    }
-    if (!data.email.trim()) {
-      errors.email = 'L\'email est requis.';
-    } else if (!/\S+@\S+\.\S+/.test(data.email.trim())) {
-      errors.email = 'L\'email n\'est pas valide.';
-    }
-    if (!data.subject.trim()) {
-      errors.subject = 'Le sujet est requis.';
-    }
-    if (!data.message.trim()) {
-      errors.message = 'Le message est requis.';
-    }
-    return errors;
-  };
-
-  const sendEmail = () => {
-    axios.post('greyyyphe@gmail.com', formData)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Nom :</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        {formErrors.name && (
-          <div className="error">{formErrors.name}</div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="email">Email :</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        {formErrors.email && (
-          <div className="error">{formErrors.email}</div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="subject">Sujet :</label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-        />
-        {formErrors.subject && (
-          <div className="error">{formErrors.subject}</div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="message">Message :</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-        {formErrors.message && (
-          <div className="error">{formErrors.message}</div>
-        )}
-      </div>
-      {formSubmitted ? (
-        <div className="success-message">
-          Votre message a bien été envoyé.
-        </div>
-      ) : (
-        <button type="submit">Envoyer</button>
-      )}
-    </form>
-  );
-};
+    <div className='container-form-parent'>
+      <div className="container-form">
+        <div className="container-speak">
+          <img src="./img/leaf-black.png" alt="leaf decoration" />
+          <h2>Parlons-en</h2>
+          <p>Cotton candy muffin cupcake sugar plum marzipan </p>
 
-export default ContactForm;
+          <div className="container-phone-mail">
+            <div className="phone">
+              <img src="./img/svg/phone.svg" alt="phone icon" />
+              <p>07 81 21 81 30</p>
+            </div>
+            <div className="mail">
+              <img src="./img/svg/mail.svg" alt="mail icon" />
+              <p></p>
+            </div>
+
+            <div className="container-form-reseau-icon">
+              <img src="./img/svg/facebook.svg" alt="facebook icon" />
+              <img src="./img/svg/instagram.svg" alt="instagram icon" />
+              <img src="./img/svg/tiktok.svg" alt="tiktok icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="container-info-form">
+          <form>
+            <div>
+              <label for="name">Nom :</label>
+              <input type="text" id="name" name="name" required/>
+            </div>
+            <div>
+              <label for="email">Email :</label>
+              <input type="email" id="email" name="email" required/>
+            </div>
+            <div>
+              <label for="message">Message :</label>
+              <textarea id="message" name="message" required></textarea>
+            </div>
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactForm
--- src/components/GallerySection/GallerySection.jsx
+++ src/components/GallerySection/GallerySection.jsx
@@ -6,9 +6,9 @@ const GallerySection = () => {
         <div className='container-gallery-parent'>
 
             <div className="container-gallery" >
-
+                <div className="background-gallery-img"> </div>
                 <div className="gallery">
-                    <div className="background-gallery-img"> </div>
+
                     <div className="container-gallery-img-parent">
                         <div className="container-gallery-img">
                             <img src="./img/img_gallery.png" alt="" />
@@ -23,8 +23,8 @@ const GallerySection = () => {
                     <div className="container-gallery-text-parent">
                         <div className="container-gallery-text">
                             <div className="container-gallery-text-line"></div>
-                                <h2>Galerie</h2>
-                                
+                            <h2>Galerie</h2>
+
                             <p>Cotton candy muffin cupcake sugar plum marzipan pie donut cotton candy. Sweet chocolate bar powder toffee sweet roll topping tiramisu marzipan. Jelly beans pie sugar plum jelly beans sesame snaps wafer. Chupa chups jelly bonbon liquorice cake.</p> <br /><br /><br />
 
                             <p>Cotton candy muffin cupcake sugar plum marzipan pie donut cotton candy. Sweet chocolate bar powder toffee sweet roll topping tiramisu marzipan. Jelly beans pie sugar plum jelly beans sesame snaps wafer. Chupa chups jelly bonbon liquorice cake.
@@ -37,7 +37,7 @@ const GallerySection = () => {
                                 </button>
                             </div>
                             <div className="container-gallery-text-line"></div>
-                          
+
                         </div>
                     </div>
 
