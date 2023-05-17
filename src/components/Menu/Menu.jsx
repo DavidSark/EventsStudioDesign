f663ac2bb8ec9fa31a96d46a44676513675d2b30
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-05-17 12:42:12 +0200

    container home height

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,53 +1,46 @@
-import React, { useRef } from 'react';
-import './Menu.scss'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from 'react';
+import './Menu.scss';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navRef = useRef();
 
-    const navRef = useRef();
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    document.body.style.overflow = isMenuOpen ? '' : 'hidden';
+  };
 
-    const showNavBar = () => {
-        navRef.current.classList.toggle("responsive_nav")
-    }
+  return (
+    <div className="menu-parent">
+      <div className="menu">
+        <Link to="/">
+          <img className="logo" src="/img/svg/ESD_Logo.svg" alt="" />
+        </Link>
+        <nav ref={navRef} className={isMenuOpen ? 'responsive_nav' : ''}>
+          <Link to="/prestations">Prestations</Link>
+          <Link>Galerie</Link>
+          <Link>L'entreprise</Link>
+          <Link>
+            <span>Contact</span>
+          </Link>
+          <Link to="/event">
+            <span>Boutique</span>
+          </Link>
+          <button
+            className="btn-nav-close nav-close-btn"
+            onClick={toggleMenu}
+          >
+            <img src="/img/svg/x.svg" alt="menu icon" />
+          </button>
+        </nav>
+        <button className="btn-nav-open" onClick={toggleMenu}>
+          <img src="/img/svg/menu.svg" alt="menu icon" />
+        </button>
+      </div>
+      <div className="separator"></div>
+    </div>
+  );
+};
 
-    return (
-        <div className="">
-            <div className='menu'>
-                <Link to="/">
-                    <img className='logo' src="/img/svg/ESD_Logo.svg" alt="" />
-                </Link>
-                <nav ref={navRef}>
-                    <Link to="/prestations">
-                        Prestations
-                    </Link>
-                    <Link>
-                        Galerie
-                    </Link>
-                    <Link>
-                        L'entreprise
-                    </Link>
-                    <Link>
-                        <span>
-                            Contact
-                        </span>
-                    </Link>
-                    <Link to="/event">
-                        <span>
-                            Boutique
-                        </span>
-                    </Link>
-
-                    <button className='btn-nav-close nav-close-btn' onClick={showNavBar}>
-                        <img src="/img/svg/x.svg" alt="menu icon" />
-                    </button>
-                </nav>
-                <button className='btn-nav-open' onClick={showNavBar}>
-                    <img src="/img/svg/menu.svg" alt="menu icon" />
-                </button>
-            </div>
-            <div className="separator"></div>
-        </div>
-    )
-}
-
-export default Menu
+export default Menu;
