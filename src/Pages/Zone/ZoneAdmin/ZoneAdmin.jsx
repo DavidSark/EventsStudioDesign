9df6bb5ebf2676e1ca33b95463196898ba941f0a
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-05-25 19:39:38 +0200

    css zone accès

--- src/Pages/Zone/ZoneAdmin/ZoneAdmin.jsx
+++ src/Pages/Zone/ZoneAdmin/ZoneAdmin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../../../components/context/AuthContext';
-
+import './ZoneAdmin.scss'
 //import des composants
 import Entete from '../../../components/Entete/Entete'
 import Menu from '../../../components/Menu/Menu'
@@ -10,23 +10,34 @@ import Menu from '../../../components/Menu/Menu'
 const ZoneAdmin = () => {
 
 
-    const { user, logout } = UserAuth();
-    const navigate = useNavigate();
-  
-    const handleLogout = async () => {
-      try {
-        await logout();
-        navigate('/');
-        console.log('Déconnecté avec succès')
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/');
+      console.log('Déconnecté avec succès')
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   return (
-    <div>
-      <Entete/>
-      <Menu/>
-      <h3>Gérez votre boutique</h3>
+    <div className='zoneadmin-main-container'>
+      <Entete />
+      <Menu />
+      <div className="zoneadmin-title">
+        <div className="zoneadmin-line"></div>
+        <h2>Bienvenue</h2>
+      </div>
+      <div className="zoneadmin-title-tagline">
+        <h2>
+          Vous êtes ici dans votre espace privé <br />
+          Gérez les produits de votre boutiques.
+        </h2>
+      </div>
+
+     
       <button onClick={handleLogout} className='border px-6 py-2 my-4'>
         Logout
       </button>
--- src/components/Entete/Entete.jsx
+++ src/components/Entete/Entete.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import {auth} from '../../config/firebase'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../config/firebase'
 import "./Entete.scss";
 
 import { onAuthStateChanged, signOut } from 'firebase/auth'
@@ -10,63 +11,65 @@ import { onAuthStateChanged, signOut } from 'firebase/auth'
 const Entete = () => {
 
   const [authUser, setAuthUser] = useState(null);
-
-  useEffect(()=>{
-    const listen = onAuthStateChanged(auth,(user) => {
-      if(user){
+  const navigate = useNavigate();
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
         setAuthUser(user)
-      }else{
+      } else {
         setAuthUser(null);
       }
     })
 
-    return () =>{
+    return () => {
       listen();
     }
-  },[]);
+  }, []);
 
 
   const logout = () => {
     signOut(auth).then(() => {
-      console.log('déconnexion réussi')
-    }).catch(error=>console.log(error));
+      console.log('déconnexion réussi');
+      navigate('/');
+    }).catch(error => console.log(error));
   }
 
-  
+
 
 
   return (
-    <div className='entete'>
-      <div className='bloc-social-icon'>
-        <a href="https://www.facebook.com/profile.php?id=100063230651252" target='_blank'>
-          <img src="/img/svg/facebook.svg" alt="Facebook icon" className="social-icon" />
-        </a>
+    <div className="entete-parent">
+      <div className='entete'>
+        <div className='bloc-social-icon'>
+          <a href="https://www.facebook.com/profile.php?id=100063230651252" target='_blank'>
+            <img src="/img/svg/facebook.svg" alt="Facebook icon" className="social-icon" />
+          </a>
 
-        <a href="https://www.instagram.com/eventsstudiodesign/" target='_blank'>
-          <img src="/img/svg/instagram.svg" alt="Instagram icon" className="social-icon" />
-        </a>
-        <a href="https://www.tiktok.com/@eventsstudiodesign" target='_blank'>
-          <img src="/img/svg/tiktok.svg" alt="Tiktok icon" className="tiktok" />
-        </a>
-      </div>
-      <div className="separator"></div>
-      <div className="entete-email">
-        <a href="mailto:evensstudiodesign@outlook.fr"><h2>evensstudiodesign@outlook.fr</h2></a>
-      </div>
-      <div className="separator"></div>
-      <div className="entete-phone">
-        <h2>07 82 75 09 19</h2>
-      </div>
-
-      
-     
-        <div>
-         {
-          authUser ? <><p>{`Connecté en tant que : ${authUser.email}`}</p> <button onClick={logout}>Déconnexion</button></> : <p>Déconnecté</p>
-         }
+          <a href="https://www.instagram.com/eventsstudiodesign/" target='_blank'>
+            <img src="/img/svg/instagram.svg" alt="Instagram icon" className="social-icon" />
+          </a>
+          <a href="https://www.tiktok.com/@eventsstudiodesign" target='_blank'>
+            <img src="/img/svg/tiktok.svg" alt="Tiktok icon" className="tiktok" />
+          </a>
         </div>
+        <div className="separator"></div>
+        <div className="entete-email">
+          <a href="mailto:evensstudiodesign@outlook.fr"><h2>evensstudiodesign@outlook.fr</h2></a>
+        </div>
+        <div className="separator"></div>
+        <div className="entete-phone">
+          <h2>07 82 75 09 19</h2>
+        </div>
+
+
+
+
+      </div>
       
-    
+      <div className='btn-logout-admin'>
+          {authUser ? <> <button onClick={logout}>Déconnexion</button></> : <p></p>}
+        </div>
+
     </div>
   )
 }
