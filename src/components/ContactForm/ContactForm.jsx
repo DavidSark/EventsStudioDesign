c10f28d97d40422a933fa5a611451fec579b7a6e
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-05-27 19:11:29 +0200

    donnée du formulaire dans firebase

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,59 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './ContactForm.scss'
 import { HashLink as Link } from 'react-router-hash-link';
+import { db } from "../../config/firebase"
+import { collection, addDoc } from "firebase/firestore";
+
 const ContactForm = () => {
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    // Vérifiez si la base de données Firestore est disponible
+    if (!db) {
+      console.error('La base de données Firestore n\'est pas disponible');
+      return;
+    }
+  
+    // Récupérez les valeurs des champs de formulaire
+    const { lastName, firstName, email, phone, address, postalCode, prestation, message } = event.target.elements;
+  
+    try {
+      // Enregistrez les données dans Firebase en utilisant addDoc
+      const docRef = await addDoc(collection(db, 'users'), {
+        lastName: lastName.value,
+        firstName: firstName.value,
+        email: email.value,
+        phone: phone.value,
+        address: address.value,
+        postalCode: postalCode.value,
+        prestation: prestation.value,
+        message: message.value
+      });
+  
+      event.target.reset();
+      console.log('Formulaire soumis avec succès ! Document ID :', docRef.id);
+    } catch (error) {
+      console.error('Erreur lors de la soumission du formulaire :', error);
+    }
+  };
+  
   return (
     <div className='container-form-parent'>
       <div className="container-form-row">
-        <div className="container-form">
-          <div className="container-speak">
-            {/* <img className='leaf-black' src="./img/svg/logo_noir.svg" alt="leaf decoration" /> */}
-            <h2>Parlons-en</h2>
-            <p>Cotton candy muffin cupcake sugar plum marzipan </p>
-
-
-          </div>
-          <div className="container-phone-mail">
-            <div className="phone">
-              <img src="./img/svg/phone.svg" alt="phone icon" />
-              <p>07 81 21 81 30</p>
-            </div>
-            <div className="mail">
-              <img src="./img/svg/mail.svg" alt="mail icon" />
-              <a href="mailto:evensstudiodesign@outlook.fr">
-                <p>evensstudiodesign@outlook.fr</p>
-              </a>
-            </div>
-          </div>
-          <div className="container-form-reseau-icon">
-            <div className="container-form-fb">
-              <img src="./img/svg/facebook-black.svg" alt="facebook icon" className="social-icon" />
-              <Link to="https://www.facebook.com/profile.php?id=100063230651252" target='_blank'>
-                <p>facebook</p>
-              </Link>
-            </div>
-            <div className="container-form-instagram">
-              <img src="./img/svg/instagram-black.svg" alt="instagram icon" className="social-icon" />
-              <Link to="https://www.instagram.com/eventsstudiodesign/" target='_blank'>
-                <p>instagram</p>
-              </Link>
-            </div>
-            <div className="container-form-tiktok">
-              <img src="./img/svg/tiktok-black.svg" alt="tiktok icon" className="tiktok" />
-              <Link to="https://www.tiktok.com/@eventsstudiodesign" target='_blank'>
-                <p>tiktok</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-
-
+      
         <div className="container-info-form">
           <div className="separation-line-form"></div>
           <div>
             <h2>Écrivez moi</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-nom-prenom">
                 <div className='form-nom'>
                   <label htmlFor="lastName">Nom<span>*</span> </label>
