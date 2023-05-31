aab7667f6542ea3a5ce1a5f65df6210bf4b2acf6
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-05-31 12:03:46 +0200

    modifié info

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,60 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ContactForm.scss'
 import { HashLink as Link } from 'react-router-hash-link';
+import { useParams } from "react-router-dom";
 import { db } from "../../config/firebase"
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 
 const ContactForm = () => {
 
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    adress: "",
-    postalCode: "",
-    prestation: "",
-    message: "",
-  }
+  //usestate pour les données
+  const [user, setUser] = useState(null);
+  const userCollectionRef = collection(db, "user");
 
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
 
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
+  //useEffect pour appeler les données de la bdd
+  useEffect(() => {
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8"
-      },
-      body: JSON.stringify(formDetails)
-    });
-    if (response.ok) {
-      try {
-        let result = await response.json();
-        setButtonText("Send");
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
-          setStatus({ success: true, message: "Message sent successfully" });
-        } else {
-          setStatus({ success: false, message: "Something went wrong" });
-        }
-      } catch (error) {
-        setStatus({ success: false, message: "Error parsing JSON response" });
-      }
-    } else {
-      setStatus({ success: false, message: "Network error" });
+    const getUser = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     }
-  }
+
+    getUser();
+  }, []);
+
+  //fonction pour modifier
+
 
   return (
     <div className='container-form-parent'>
@@ -62,38 +32,49 @@ const ContactForm = () => {
 
         <div className="container-form">
           <div className="container-speak">
-            {/* <img className='leaf-black' src="./img/svg/logo_noir.svg" alt="leaf decoration" /> */}
             <h2>Parlons-en</h2>
             <p>Cotton candy muffin cupcake sugar plum marzipan </p>
           </div>
           <div className="container-phone-mail">
             <div className="phone">
               <img src="./img/svg/phone.svg" alt="phone icon" />
-              <p>07 81 21 81 30</p>
+
+              {user && user.map((userData) => (
+                <div className="phone" key={userData.id}>
+                  <p>{userData.phone}</p>
+                </div>
+              ))}
+
             </div>
             <div className="mail">
               <img src="./img/svg/mail.svg" alt="mail icon" />
-              <a href="mailto:evensstudiodesign@outlook.fr">
-                <p>evensstudiodesign@outlook.fr</p>
-              </a>
+              
+              {user && user.map((userData) => (
+                <a href={`mailto:${userData.email}`}>
+                <div className="phone" key={userData.id}>
+                  <p>{userData.email}</p>
+                </div>
+                </a>
+              ))}
+              
             </div>
           </div>
           <div className="container-form-reseau-icon">
             <div className="container-form-fb">
               <img src="./img/svg/facebook-black.svg" alt="facebook icon" className="social-icon" />
-              <Link to="https://www.facebook.com/profile.php?id=100063230651252" target='_blank'>
+              <Link to="https://www.facebook.com/" target='_blank'>
                 <p>facebook</p>
               </Link>
             </div>
             <div className="container-form-instagram">
               <img src="./img/svg/instagram-black.svg" alt="instagram icon" className="social-icon" />
-              <Link to="https://www.instagram.com/eventsstudiodesign/" target='_blank'>
+              <Link to="https://www.instagram.com/" target='_blank'>
                 <p>instagram</p>
               </Link>
             </div>
             <div className="container-form-tiktok">
               <img src="./img/svg/tiktok-black.svg" alt="tiktok icon" className="tiktok" />
-              <Link to="https://www.tiktok.com/@eventsstudiodesign" target='_blank'>
+              <Link to="https://www.tiktok.com/" target='_blank'>
                 <p>tiktok</p>
               </Link>
             </div>
@@ -104,44 +85,44 @@ const ContactForm = () => {
           <div>
             <h2>Écrivez moi</h2>
             <form action="https://api.web3forms.com/submit" method="POST">
-            <input type="hidden" name="access_key" value="6c8caee3-995b-4ff4-a909-21f8e3595ce2"/>
-            <input type="hidden" name="redirect" value=""/>
+              <input type="hidden" name="access_key" value="6c8caee3-995b-4ff4-a909-21f8e3595ce2" />
+              <input type="hidden" name="redirect" value="" />
               <div className="form-nom-prenom">
                 <div className='form-nom'>
                   <label htmlFor="lastName">Nom<span>*</span> </label>
-                  <input type="text" value={formDetails.lastName} id="lastName" name="lastName" required placeholder='Votre nom' onChange={(e) => onFormUpdate("lastName", e.target.value)} />
+                  <input type="text" id="lastName" name="lastName" required placeholder='Votre nom' />
                 </div>
                 <div className='form-prenom'>
                   <label htmlFor="firstName" placeholder='prenom'>Prénom<span>*</span></label>
-                  <input type="text" value={formDetails.firstName} id="firstName" name="firstName" required placeholder='Votre prénom' onChange={(e) => onFormUpdate("firstName", e.target.value)} />
+                  <input type="text" id="firstName" name="firstName" required placeholder='Votre prénom' />
                 </div>
               </div>
 
               <div className="form-mail-phone">
                 <div className='form-mail'>
                   <label htmlFor="email">Email<span>*</span></label>
-                  <input type="email" value={formDetails.email} id="email" name="email" required placeholder='Votre prénom' onChange={(e) => onFormUpdate("email", e.target.value)} />
+                  <input type="email" id="email" name="email" required placeholder='Votre prénom' />
                 </div>
                 <div className='form-phone'>
                   <label htmlFor="phone">Téléphone</label>
-                  <input type="tel" value={formDetails.phone} id="phone" name="phone" placeholder='01 23 45 67 89'
-                    onChange={(e) => onFormUpdate("phone", e.target.value)} />
+                  <input type="tel" id="phone" name="phone" placeholder='01 23 45 67 89'
+                  />
                 </div>
               </div>
 
               <div className="form-adress-postal">
                 <div className='form-adress'>
                   <label htmlFor="address">Adresse complète<span>*</span></label>
-                  <input type="text" value={formDetails.adress} id="address" name="address" required placeholder='123 rue exemple' onChange={(e) => onFormUpdate("adress", e.target.value)} />
+                  <input type="text" id="address" name="address" required placeholder='123 rue exemple' />
                 </div>
                 <div className='form-postal'>
                   <label htmlFor="postalCode">Code postal<span>*</span></label>
-                  <input type="text" value={formDetails.postalCode} id="postalCode" name="postalCode" required placeholder='51100' onChange={(e) => onFormUpdate("postalCode", e.target.value)} />
+                  <input type="text" id="postalCode" name="postalCode" required placeholder='51100' />
                 </div>
               </div>
               <div className='form-prestation'>
                 <label htmlFor="prestation">Prestation<span>*</span></label>
-                <select id="prestation" value={formDetails.prestation} name="prestation" required onChange={(e) => onFormUpdate("prestation", e.target.value)}>
+                <select id="prestation" name="prestation" required >
                   <option value="">Sélectionnez une prestation</option>
                   <option value="mariage">Mariage</option>
                   <option value="anniversaire">Anniversaire</option>
@@ -151,16 +132,10 @@ const ContactForm = () => {
               </div>
               <div className='form-message'>
                 <label htmlFor="message">Message<span>*</span></label>
-                <textarea id="message" value={formDetails.message} name="message" required placeholder='Votre message' onChange={(e) => onFormUpdate("message", e.target.value)}></textarea>
+                <textarea id="message" name="message" required placeholder='Votre message'></textarea>
               </div>
-              <button type="submit" className='form-send-btn'>{buttonText}</button>
-              {status.message && (
-                <div className="row">
-                  <p className={status.success === false ? "danger" : "success"}>
-                    {status.message}
-                  </p>
-                </div>
-              )}
+              <button type="submit" className='form-send-btn'>Envoyer</button>
+
             </form>
           </div>
         </div>
--- src/components/Entete/Entete.jsx
+++ src/components/Entete/Entete.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../config/firebase'
 import "./Entete.scss";
-
+import { db } from "../../config/firebase"
+import { collection, addDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 // import Facebook from '../../../public/img/svg/facebook.svg'
 // import Instagram from '../../../public/img/svg/instagram.svg'
@@ -34,7 +35,46 @@ const Entete = () => {
     }).catch(error => console.log(error));
   }
 
+  //récupérer et modifier les données mobiles et email : 
 
+  //usestate pour les données
+  const [user, setUser] = useState(null);
+  const userCollectionRef = collection(db, "user");
+  const [editPhone, setEditPhone] = useState(false);
+  const [editEmail, setEditEmail] = useState(false);
+  //useEffect pour appeler les données de la bdd
+  useEffect(() => {
+
+    const getUser = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+
+    getUser();
+  }, []);
+
+  //fonction pour modifier
+  const updateUserPhone = async () => {
+    try {
+      const userDocRef = doc(db, 'user', user[0].id);
+      await updateDoc(userDocRef, { phone: user[0].phone });
+      setEditPhone(false);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateUserEmail = async () => {
+    try {
+      const userDocRef = doc(db, 'user', user[0].id);
+      await updateDoc(userDocRef, { email: user[0].email });
+      setEditEmail(false);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
   return (
@@ -54,21 +94,56 @@ const Entete = () => {
         </div>
         <div className="separator"></div>
         <div className="entete-email">
-          <a href="mailto:evensstudiodesign@outlook.fr"><h2>evensstudiodesign@outlook.fr</h2></a>
+          {user && !editEmail ? (
+            <div className="entete-email">
+              <a href={`mailto:${user[0].email}`}><h2>{user[0].email}</h2></a>
+              {authUser ? <> <button onClick={() => setEditEmail(true)}>+</button></> : <p></p>}
+             
+            </div>
+          ) : (
+            <div>
+              <input
+                type="email"
+                value={user ? user[0].email : ''}
+                onChange={(e) => {
+                  setUser((prevUser) => [{ ...prevUser[0], email: e.target.value }]);
+                }}
+              />
+              <button onClick={updateUserEmail}>Enregistrer</button>
+            </div>
+          )}
         </div>
         <div className="separator"></div>
         <div className="entete-phone">
-          <h2>07 82 75 09 19</h2>
+          {user && !editPhone ? (
+            <div>
+              <h2>{user[0].phone}</h2>
+              {authUser? <><button onClick={() => setEditPhone(true)}>+</button></> : <p></p>}
+              
+            </div>
+          ) : (
+            <div>
+              <input
+                type="tel"
+                value={user ? user[0].phone : ''}
+                onChange={(e) => {
+                  setUser((prevUser) => [{ ...prevUser[0], phone: e.target.value }]);
+                }}
+              />
+              <button onClick={updateUserPhone}>Enregistrer</button>
+              
+            </div>
+          )}
         </div>
 
 
 
 
       </div>
-      
+
       <div className='btn-logout-admin'>
-          {authUser ? <> <button onClick={logout}>Déconnexion</button></> : <p></p>}
-        </div>
+        {authUser ? <> <button onClick={logout}>Déconnexion</button></> : <p></p>}
+      </div>
 
     </div>
   )
