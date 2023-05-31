64f26546781e5cf24346f4ef4abc6ccd37f225cd
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-06-01 01:31:14 +0200

    boutique single

--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -3,6 +3,10 @@ import "./Single.scss";
 import { useParams } from "react-router-dom";
 import { db } from "../../config/firebase";
 import { collection, doc, getDoc } from "firebase/firestore";
+import Entete from "../../components/Entete/Entete";
+import Menu from "../../components/Menu/Menu";
+
+
 
 const Single = () => {
   const { productId } = useParams();
@@ -32,10 +36,33 @@ const Single = () => {
 
   return (
     <div>
-      <h2>Product Page</h2>
-      <p>Product ID: {product.id}</p>
-      <p>Prix: {product.prix}</p>
-      <img src={product.img} alt={product.name} />
+      <Entete />
+      <Menu />
+      <div className="prestations-title-gallery">
+        <div className="prestations-line-gallery"></div>
+        <h2>{product.nom}</h2>
+      </div>
+      <div className="container-single">
+
+        <div className="container-single-left">
+          <img src={product.img} alt={product.name} />
+        </div>
+
+        <div className="container-single-right">
+          <h2>{product.nom}</h2>
+          <p className="container-single-price">Prix: {product.prix}€</p>
+          <div className="single-line"></div>
+          <p className="container-single-desc">{product.description}</p>
+          <div className="single-line"></div>
+          <p>Ce produit vous intéresse ?</p>
+          <span>Contactez moi !</span>
+          <div className="single-line"></div>
+          <p>Tag: {product.categorie}</p>
+
+        </div>
+
+
+      </div>
     </div>
   );
 };
